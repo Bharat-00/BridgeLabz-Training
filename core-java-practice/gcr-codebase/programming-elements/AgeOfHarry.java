9552974b01863dd5d248a0ca15d5cd45cbd98d88
Bharat-00 <bharat.trivedi_cs22@gla.ac.in>
2025-12-18 15:38:07 +0530

    [Bharat]PE

---
+++ core-java-practice/gcr-codebase/programming-elements/AgeOfHarry.java
@@ -0,0 +1,9 @@
+public class AgeOfHarry {
+    public static void main(String[] args) {
+        int birthYear=2000;
+        int currentYear=2024;
+        int age=currentYear-birthYear;
+        System.err.println("Age of harry in 2024 is:"+age);
+    }
+    
+}
