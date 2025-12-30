0e89f2e81e2f69ad88beabacc3ad354260346c6f
Bharat-00 <bharat.trivedi_cs22@gla.ac.in>
2025-12-30 15:41:55 +0530

    Clean main branch: keep only README

--- core-java-practice/gcr-codebase/programming-elements/KmToMiles.java
+++
@@ -1,9 +0,0 @@
-
-public class KmToMiles {
-    public static void main(String[] args) {
-        double distanceInKm=10.8;
-        double distanceInMiles=distanceInKm*0.621371;
-        System.err.println("The distance"+distanceInKm+"km in miles is:"+distanceInMiles);
-    }
-    
-}
