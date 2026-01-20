1d3258ee14eb0de547fb5b1f0c89817de8c2a1c4
Bharat-00 <bharat.trivedi_cs22@gla.ac.in>
2026-01-20 11:19:19 +0530

    [Bharat]Add.added the leet-code  questions.

---
+++ dsa-practice/leet-code-codebase/ArrangeCoins.java
@@ -0,0 +1,10 @@
+public class ArrangeCoins {
+    public int arrangeCoins(int n) {
+        int rows = 0;
+        while (n >= rows + 1) {
+            rows++;
+            n -= rows;
+        }
+        return rows;
+    }
+}
