5b1494606c812b041cfb831e4f7782dc1c9f9846
Bharat-00 <bharat.trivedi_cs22@gla.ac.in>
2026-02-03 11:11:12 +0530

    [Bharat]Add.added the new io programming branch.

---
+++ data-handling-practice/gcr-codebase/csv-data-handling/FilterStudents.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class FilterStudents {
+	public static void main(String[] args) {
+		String filePath = "data-handling-practice\\gcr-codebase\\csv-data-handling\\students.csv";
+		String line;
+		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+			br.readLine();
+			System.out.println("Students scoring more than 80: ");
+			System.out.println(" ");
+			while ((line = br.readLine()) != null) {
+				String[] data = line.split(",");
+				int marks = Integer.parseInt(data[3]);
+				if (marks > 80) {
+					System.out.println("ID: " + data[0] + ", Name: " + data[1] + ", Marks: " + marks);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
